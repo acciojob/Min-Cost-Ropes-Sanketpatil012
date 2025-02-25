9b873822bd44b689b53dae12401d90bf79a9b3ee
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-02-25 15:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function mincost(arr)
 { 
-//write your code here
+    arr.sort((a ,b) => a-b)
+	 
+	  let totalCost=0 ;
+	while (arr.length>1) {
+   
+	  let first= arr.shift();
+	  let second= arr.shift();
+
+	  let  cost= first + second; 
+           totalCost+=cost 
+		arr.push(cost)
+      arr.sort((a,b) => a- b )
+	   
+	}
+	
+	return totalCost
+	//write your code here
 // return the min cost
   
 }
